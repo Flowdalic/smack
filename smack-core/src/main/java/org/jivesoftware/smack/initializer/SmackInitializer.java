97bc33635d39f41b1707ce821c948a179133942e
Christoph Fiehe <christoph.fiehe@materna.de>
2014-12-18 12:57:21 +0100

    Enable OSGi compliance on package layer via 'DynamicImport-Package: *' instead of Declarative Service (DS) approach. Restructuring and cleanup of initialization process to ensure that all internal config files are found by the corresponding bundle classloaders.
    SMACK-343

--- smack-core/src/main/java/org/jivesoftware/smack/initializer/SmackInitializer.java
+++ smack-core/src/main/java/org/jivesoftware/smack/initializer/SmackInitializer.java
@@ -30,6 +30,7 @@ import org.jivesoftware.smack.SmackConfiguration;
  *
  */
 public interface SmackInitializer {
+	
     public List<Exception> initialize();
-    public List<Exception> initialize(ClassLoader classLoader);
+    
 }
